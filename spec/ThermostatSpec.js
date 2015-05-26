2f3b410446bf5cd2f838ee67762fca7ff89915b1
Ptolemy <ptolemybarnes@gmail.com>
2015-05-26 13:37:12 +0100

    Change spec to more unit-testy style

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,20 +5,40 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  it('registers an increase in desired temperature', function() {
-    var startingTemp = thermostat.temperature;
+  describe('#increaseTemperature', function() {
 
-    thermostat.increaseTemperature();
+    it('registers an increase in desired temperature', function() {
+      var startingTemp = thermostat.temperature;
 
-    expect(thermostat.temperature > startingTemp).toBe(true);
+      thermostat.increaseTemperature();
+
+      expect(thermostat.temperature > startingTemp).toBe(true);
+    });
+
+    it('raises error if temperature is increased above maximum', function() {
+      thermostat.temperature = thermostat.maximumBound; 
+
+      expect( function() { thermostat.increaseTemperature() })
+        .toThrow('Temperature out of bounds');
+    });
   });
 
-  it('registers a decrease in desired temperature', function() { 
-    var startingTemp = thermostat.temperature;
+  describe('#decreaseTemperature', function() {
+     
+    it('registers a decrease in desired temperature', function() { 
+      var startingTemp = thermostat.temperature;
 
-    thermostat.decreaseTemperature();
-  
-    expect(thermostat.temperature < startingTemp).toBe(true);
+      thermostat.decreaseTemperature();
+    
+      expect(thermostat.temperature < startingTemp).toBe(true);
+    });
+    
+    it('raises error if temperature is lowered below minimum', function() {
+      thermostat.temperature = thermostat.minimumBound; 
+
+      expect( function() { thermostat.decreaseTemperature() })
+        .toThrow('Temperature out of bounds');
+    });
   });
 
   describe('#resetTemperature', function() {
@@ -31,26 +51,7 @@ describe('Thermostat', function() {
     });
   });
 
-  describe('when temperature is lowered below the minimum bound', function() {
-    
-    it('raises an out-of-bound error', function() {
-      thermostat.temperature = thermostat.minimumBound; 
-
-      expect( function() { thermostat.decreaseTemperature() })
-        .toThrow('Temperature out of bounds');
-    });
-  });
-
-  describe('when temperature is raised above maximum bound', function() {
-    it('raises an out-of-bounds error', function() {
-      thermostat.temperature = thermostat.maximumBound; 
-
-      expect( function() { thermostat.increaseTemperature() })
-        .toThrow('Temperature out of bounds');
-    });
-  });
-
-  describe('when power save mode is turned on', function() { 
+  describe('#turnOnPowerSave', function() { 
     it('lowers maximum bound to 25', function() {
       thermostat.turnOnPowerSave();
 
@@ -66,13 +67,14 @@ describe('Thermostat', function() {
     });
   });
 
-  describe('when power save mode is turned off', function() { 
+  describe('#turnOffPowerSave', function() { 
+    
     it('raises maximum bound to its default', function() {
       thermostat.turnOnPowerSave();
       thermostat.turnOffPowerSave();
 
       expect(thermostat.maximumBound).toEqual(thermostat.defaultMaximumBound);
-    })
+    });
   });
 });
 
